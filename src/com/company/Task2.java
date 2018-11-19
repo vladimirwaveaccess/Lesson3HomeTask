aaa67c451e7a8049ef737269888e6116887d4163
Volodymyr Ahafonov <volodymyr.ahafonov@globallogic.com>
2018-11-19 12:43:28 +0200

    Added 2 classes, Task1 and Task2

---
+++ src/com/company/Task2.java
@@ -0,0 +1,227 @@
+package com.company;
+
+class Task2 {
+    // -------------------------------------- START: Transformation from byte ----------------------------------------------
+    static short transformationByteToShort(byte x) {
+        short rez = x;
+        return rez;
+    }
+
+    static char transformationByteToChar(byte x) {
+        char rez = (char) x;
+        return rez;
+    }
+
+    static int transformationByteToInt(byte x) {
+        int rez =  x;
+        return rez;
+    }
+
+    static long transformationByteToLong(byte x) {
+        long rez =  x;
+        return rez;
+    }
+
+    static float transformationByteToFloat(byte x) {
+        float rez =  x;
+        return rez;
+    }
+
+    static double transformationByteToDouble(byte x) {
+        double rez =  x;
+        return rez;
+    }
+// --------------------------------------- END: Transformation from byte -----------------------------------------------
+
+// ------------------------------------- START: Transformation from short ----------------------------------------------
+    static byte transformationShortToByte(short x) {
+        byte rez = (byte) x;
+        return rez;
+    }
+
+    static char transformationShortToChar(short x) {
+        char rez = (char) x;
+        return rez;
+    }
+
+    static int transformationShortToInt(short x) {
+        int rez = x;
+        return rez;
+    }
+
+    static long transformationShortToLong(short x) {
+        long rez = x;
+        return rez;
+    }
+
+    static float transformationShortToFloat(short x) {
+        float rez = x;
+        return rez;
+    }
+
+    static double transformationShortToDouble(short x) {
+        double rez = x;
+        return rez;
+    }
+// -------------------------------------- END: Transformation from short -----------------------------------------------
+
+// -------------------------------------- START: Transformation from char ----------------------------------------------
+    static byte transformationCharToByte(char x) {
+        byte rez = (byte) x;
+        return rez;
+    }
+
+    static short transformationCharToShort(char x) {
+        short rez = (short) x;
+        return rez;
+    }
+
+    static int transformationCharToInt(char x) {
+        int rez = x;
+        return rez;
+    }
+
+    static long transformationCharToLong(char x) {
+        long rez = x;
+        return rez;
+    }
+
+    static float transformationCharToFloat(char x) {
+        float rez = x;
+        return rez;
+    }
+
+    static double transformationCharToDouble(char x) {
+        double rez = x;
+        return rez;
+    }
+// --------------------------------------- END: Transformation from char -----------------------------------------------
+
+// --------------------------------------- START: Transformation from int ----------------------------------------------
+    static byte transformationIntToByte(int x) {
+        byte rez = (byte) x;
+        return rez;
+    }
+
+    static short transformationIntToShort(int x) {
+        short rez = (short) x;
+        return rez;
+    }
+
+    static char transformationIntToChar(int x) {
+        char rez = (char) x;
+        return rez;
+    }
+
+    static long transformationIntToLong(int x) {
+        long rez = x;
+        return rez;
+    }
+
+    static float transformationIntToFloat(int x) {
+        float rez = x;
+        return rez;
+    }
+
+    static double transformationIntToDouble(int x) {
+        double rez = x;
+        return rez;
+    }
+// ---------------------------------------- END: Transformation from int -----------------------------------------------
+
+// -------------------------------------- START: Transformation from long ----------------------------------------------
+    static byte transformationLongToByte(long x) {
+        byte rez = (byte) x;
+        return rez;
+    }
+
+    static short transformationLongToShort(long x) {
+        short rez = (short) x;
+        return rez;
+    }
+
+    static char transformationLongToChar(long x) {
+        char rez = (char) x;
+        return rez;
+    }
+
+    static int transformationLongToInt(long x) {
+        int rez = (int) x;
+        return rez;
+    }
+
+    static float transformationLongToFloat(long x) {
+        float rez = x;
+        return rez;
+    }
+
+    static double transformationLongToDouble(long x) {
+        double rez = x;
+        return rez;
+    }
+// --------------------------------------- END: Transformation from long -----------------------------------------------
+
+// -------------------------------------- START: Transformation from float ---------------------------------------------
+    static byte transformationFloatToByte(float x) {
+        byte rez = (byte) x;
+        return rez;
+    }
+
+    static short transformationFloatToShort(float x) {
+        short rez = (short) x;
+        return rez;
+    }
+
+    static char transformationFloatToChar(float x) {
+        char rez = (char) x;
+        return rez;
+    }
+
+    static int transformationFloatToInt(float x) {
+        int rez = (int) x;
+        return rez;
+    }
+
+    static long transformationFloatToLong(float x) {
+        long rez = (long) x;
+        return rez;
+    }
+
+    static double transformationFloatToDouble(float x) {
+        double rez = x;
+        return rez;
+    }
+// --------------------------------------- END: Transformation from float ----------------------------------------------
+
+// ------------------------------------- START: Transformation from double ---------------------------------------------
+    static byte transformationDoubleToByte(double x) {
+        byte rez = (byte) x;
+        return rez;
+    }
+
+    static short transformationDoubleToShort(double x) {
+        short rez = (short) x;
+        return rez;
+    }
+
+    static char transformationDoubleToChar(double x) {
+        char rez = (char) x;
+        return rez;
+    }
+
+    static int transformationDoubleToInt(double x) {
+        int rez = (int) x;
+        return rez;
+    }
+
+    static long transformationDoubleToLong(double x) {
+        long rez = (long) x;
+        return rez;
+    }
+
+    static float transformationDoubleToFloat(double x) {
+        float rez = (float) x;
+        return rez;
+    }
+// -------------------------------------- END: Transformation from double ----------------------------------------------
+}
